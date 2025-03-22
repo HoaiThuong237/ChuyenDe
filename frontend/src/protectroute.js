e3d97b94c85d160811e75ed7f2457a673d97b641
HoaiThuong237 <thuongpham425@gmail.com>
2025-03-22 10:53:43 +0700

    thiet lap sql dang nhap va dang ky

--- frontend/src/protectroute.js
+++ frontend/src/protectroute.js
@@ -1,8 +1,26 @@
+// import { Navigate } from "react-router-dom";
+
+// const ProtectedRoute = ({ children }) => {
+//     const isLoggedIn = localStorage.getItem("token");
+//     return isLoggedIn ? children : <Navigate to="/login" />;
+// };
+
+// export default ProtectedRoute;
+    
+import React from "react";
 import { Navigate } from "react-router-dom";
 
+// const ProtectedRoute = ({ children }) => {
+//     const isLoggedIn = localStorage.getItem("rememberLogin") === "true";
+
+//     return isLoggedIn ? children : <Navigate to="/login" />;
+// };
+
+
 const ProtectedRoute = ({ children }) => {
-    const isLoggedIn = localStorage.getItem("token");
-    return isLoggedIn ? children : <Navigate to="/login" />;
+    const isAuthenticated = localStorage.getItem("user") !== null || localStorage.getItem("rememberLogin") === "true";
+
+    return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
